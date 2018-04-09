bb432fb7fd84200176b8741e63652ba6a32f4e1b
Pavel Dobranov <pdobranov@gmail.com>
2018-04-09 22:30:15 +0300

    Added car services state container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Provider } from 'unstated';
 
 import RootNavigation from './navigation/RootNavigation';
 
 const App = () => (
-  <RootNavigation />
+  <Provider>
+    <RootNavigation />
+  </Provider>
 );
 
 export default App;
--- src/screens/CarServices.js
+++ src/screens/CarServices.js
@@ -1,48 +1,8 @@
 import React from 'react';
 import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { Subscribe } from 'unstated';
 
-const services = [
-  {
-    "id": 9,
-    "name": "ГТП"
-  },
-  {
-    "id": 14,
-    "name": "Смяна на гуми"
-  },
-  {
-    "id": 15,
-    "name": "Хотел за гуми",
-  },
-  {
-    "id": 13,
-    "name": "Автоклиматици",
-  },
-  {
-    "id": 10,
-    "name": "Реглаж",
-  },
-  {
-    "id": 11,
-    "name": "Сервиз",
-  },
-  {
-    "id": 16,
-    "name": "Автомивка",
-  },
-  {
-    "id": 12,
-    "name": "Компютърна Диагностика",
-  },
-  {
-    "id": 18,
-    "name": "Застраховки",
-  },
-  {
-    "id": 17,
-    "name": "Закарване на автомобил до оторизиран сервиз / сервиз по желание на клиента",
-  }
-];
+import CarServicesContainer from '../containers/CarServicesContainer';
 
 const styles = StyleSheet.create({
   container: {
@@ -58,13 +18,17 @@ const styles = StyleSheet.create({
 });
 
 const CarServices = () => (
-  <View style={styles.container}>
-    <FlatList
-      data={services}
-      renderItem={({ item }) => <Text style={styles.listItem}>{item.name}</Text>}
-      keyExtractor={({ id }) => String(id)}
-    />
-  </View>
+  <Subscribe to={[CarServicesContainer]}>
+    {CarSvcs => (
+      <View style={styles.container}>
+        <FlatList
+          data={CarSvcs.state.services}
+          renderItem={({ item }) => <Text style={styles.listItem}>{item.name}</Text>}
+          keyExtractor={({ id }) => String(id)}
+        />
+      </View>
+    )}
+  </Subscribe>
 );
 
 export default CarServices;
