ed5a767d2dcfaa13497455a192c0c94f7b0999f9
Pavel Dobranov <pdobranov@gmail.com>
2018-04-20 07:45:26 +0300

    CarServicesContainer fetching data from remote api

--- src/containers/CarServicesContainer.js
+++ src/containers/CarServicesContainer.js
@@ -1,48 +1,21 @@
 import { Container } from 'unstated';
 
 export default class CarServicesContainer extends Container {
+  constructor() {
+    super();
+    this.fetchServices();
+  }
+
   state = {
-    services: [
-      {
-        "id": 9,
-        "name": "ГТП"
-      },
-      {
-        "id": 14,
-        "name": "Смяна на гуми"
-      },
-      {
-        "id": 15,
-        "name": "Хотел за гуми",
-      },
-      {
-        "id": 13,
-        "name": "Автоклиматици",
-      },
-      {
-        "id": 10,
-        "name": "Реглаж",
-      },
-      {
-        "id": 11,
-        "name": "Сервиз",
-      },
-      {
-        "id": 16,
-        "name": "Автомивка",
-      },
-      {
-        "id": 12,
-        "name": "Компютърна Диагностика",
-      },
-      {
-        "id": 18,
-        "name": "Застраховки",
-      },
-      {
-        "id": 17,
-        "name": "Закарване на автомобил до оторизиран сервиз / сервиз по желание на клиента",
-      }
-    ]
+    services: [],
+    loading: true,
+  };
+
+  fetchServices = () => {
+    fetch('http://cloudcarservice.herokuapp.com/services')
+      .then(response => response.json())
+      .then(services => {
+        this.setState({ services, loading: false });
+      });
   };
 }
