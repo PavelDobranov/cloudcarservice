1e01c61c570a228060d502f85f9c7925141e713a
Pavel Dobranov <pdobranov@gmail.com>
2018-04-23 23:11:31 +0300

    Normalize api response

--- src/containers/CarServicesContainer.js
+++ src/containers/CarServicesContainer.js
@@ -1,5 +1,30 @@
 import { Container } from 'unstated';
 
+const normalizeServicesData = (services) => {
+  return services.map(service => {
+    const { id, name, title, children, products, price } = service;
+
+    const result = {
+      id,
+      title: title || name
+    };
+
+    if (children && children.length) {
+      result.children = normalizeServicesData(children);
+    }
+
+    if (products && products.length) {
+      result.children = normalizeServicesData(products);
+    }
+
+    if (price) {
+      result.price = price;
+    }
+
+    return result;
+  });
+};
+
 export default class CarServicesContainer extends Container {
   constructor() {
     super();
@@ -8,14 +33,15 @@ export default class CarServicesContainer extends Container {
 
   state = {
     services: [],
-    loading: true,
   };
 
   fetchServices = () => {
     fetch('http://cloudcarservice.herokuapp.com/services')
       .then(response => response.json())
       .then(services => {
-        this.setState({ services, loading: false });
+        const normlized = normalizeServicesData(services);
+
+        this.setState({ services: normlized });
       });
   };
 }
