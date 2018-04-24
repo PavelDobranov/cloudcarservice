4b1d706ba72d0891fb84e051c74a4cb4cd591a6c
Pavel Dobranov <pdobranov@gmail.com>
2018-04-24 22:41:34 +0300

    Added eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   "parser": "babel-eslint",
   "extends": "airbnb",
   "rules": {
+    "arrow-parens": 0,
+    "object-curly-newline": 0,
     "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0 }],
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
   }
