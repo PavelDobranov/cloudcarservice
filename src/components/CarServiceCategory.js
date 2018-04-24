84299e5b33dc73f68c33f947a1d38f25df511252
Pavel Dobranov <pdobranov@gmail.com>
2018-04-24 23:35:21 +0300

    Added styles and image icons

--- src/components/CarServiceCategory.js
+++ src/components/CarServiceCategory.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { View, TouchableOpacity, Text, Image, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    margin: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: 10,
+    backgroundColor: '#fff',
+    borderRadius: 2,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#dddfe2',
+  },
+  textContainer: {
     padding: 10,
+    width: '80%',
   },
 });
 
-const CarServiceCategory = ({ title, onPress }) => (
+const CarServiceCategory = ({ category, onPress }) => (
   <TouchableOpacity onPress={onPress} style={styles.container}>
-    <Text>{title}</Text>
+    <View style={styles.textContainer}>
+      <Text>{category.title}</Text>
+    </View>
+    {
+      category.image ?
+        (<Image
+          style={{ width: 80, height: 80 }}
+          source={{ uri: `https://cloudcarservice.com/img/service_icons/${category.image}` }}
+        />) :
+        (<Image
+          style={{ width: 20, height: 20, margin: 20 }}
+          source={{ uri: 'http://vnsoftindia.com/anagram/img/arrow-25-xxl.png' }}
+        />)
+    }
   </TouchableOpacity>
 );
 
