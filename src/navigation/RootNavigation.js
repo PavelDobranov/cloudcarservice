9ac1fbc8cf5d8af8d86aa2fae2c6be97efbbcba5
Pavel Dobranov <pdobranov@gmail.com>
2018-04-29 22:56:56 +0300

    Added styles to root navigation

--- src/navigation/RootNavigation.js
+++ src/navigation/RootNavigation.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { TabNavigator } from 'react-navigation';
+import IonIcon from 'react-native-vector-icons/Ionicons';
 
 import CarServicesStack from './CarServicesStack';
 import ShoppingCartStack from './ShoppingCartStack';
@@ -9,18 +11,21 @@ const routes = {
     screen: CarServicesStack,
     navigationOptions: {
       title: 'Услуги',
+      tabBarIcon: ({ tintColor }) => <IonIcon name="ios-car" size={30} color={tintColor} />,
     },
   },
   ShoppingCart: {
     screen: ShoppingCartStack,
     navigationOptions: {
       title: 'Кошница',
+      tabBarIcon: ({ tintColor }) => <IonIcon name="ios-basket" size={30} color={tintColor} />,
     },
   },
   UserProfile: {
     screen: UserProfileStack,
     navigationOptions: {
       title: 'Профил',
+      tabBarIcon: ({ tintColor }) => <IonIcon name="ios-contact" size={30} color={tintColor} />,
     },
   },
 };
@@ -28,6 +33,28 @@ const routes = {
 const config = {
   tabBarPosition: 'bottom',
   swipeEnabled: false,
+  tabBarOptions: {
+    pressColor: 'darkgrey',
+    style: {
+      backgroundColor: '#ff6633',
+    },
+    tabStyle: {
+      padding: 0,
+    },
+    labelStyle: {
+      marginTop: 0,
+      marginBottom: 8,
+      fontSize: 10,
+    },
+    indicatorStyle: {
+      backgroundColor: '#fff',
+    },
+    iconStyle: {
+      width: 40,
+      height: 30,
+    },
+    showIcon: true,
+  },
 };
 
 export default TabNavigator(routes, config);
