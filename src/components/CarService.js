26a263525857adf320f0f7258d211e1a48e8ad0e
Pavel Dobranov <pdobranov@gmail.com>
2018-05-09 23:26:01 +0300

    Uptate layout and styles

--- src/components/CarService.js
+++ src/components/CarService.js
@@ -1,37 +1,55 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import IonIcon from 'react-native-vector-icons/Ionicons';
 
 import withShoppingCartState from '../containers/withShoppingCartState';
 
 const styles = StyleSheet.create({
   container: {
     margin: 10,
-    padding: 20,
+    padding: 10,
+    backgroundColor: '#fff',
     borderRadius: 2,
     borderWidth: StyleSheet.hairlineWidth,
     borderColor: '#dddfe2',
-    backgroundColor: '#fff',
+  },
+  top: {
+    marginBottom: 5,
+    paddingBottom: 5,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: '#dddfe2',
+  },
+  bottom: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  price: {
+    fontWeight: 'bold',
   },
   button: {
-    margin: 10,
-    padding: 10,
-    justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 2,
-    backgroundColor: 'orange',
+    justifyContent: 'center',
+    padding: 4,
   },
 });
 
 const CarService = ({ service, shoppingCart }) => (
   <View style={styles.container}>
-    <Text>{service.title}</Text>
-    <Text>{service.price.toFixed(2)} лв.</Text>
-    <TouchableOpacity
-      style={styles.button}
-      onPress={() => shoppingCart.addProduct(service.id)}
-    >
-      <Text>Добави</Text>
-    </TouchableOpacity>
+    <View style={styles.top}>
+      <Text>{service.title}</Text>
+    </View>
+    <View style={styles.bottom}>
+      <Text style={styles.price}>
+        {service.price ? `Цена: ${service.price.toFixed(2)} лв.` : 'Направи запиване'}
+      </Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => shoppingCart.addProduct(service)}
+      >
+        <IonIcon name="ios-basket" size={40} color="green" />
+      </TouchableOpacity>
+    </View>
   </View>
 );
 
